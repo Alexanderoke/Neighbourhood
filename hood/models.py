19bb0e0d03d0c148455a24e91bfd55bbb9c7c137
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-22 06:15:45 +0300

    create methods for neighborhoood model

--- hood/models.py
+++ hood/models.py
@@ -7,3 +7,22 @@ class NeighbourHood(models.Model):
   NeighbourHoodLocation = models.CharField(max_length=200)
   OccupantsCount = models.IntegerField()
   admin = models.ForeignKey(User, on_delete=models.CASCADE, related_NeighbourhoodName="hood",null=True)
+
+
+def __str__(self):
+  return self.NeighbourhoodName
+
+def create_neighborhood(self):
+    self.save()
+
+def delete_neighborhood(self):
+    self.delete()
+        
+def update_neighborhood(self):
+     self.update()
+def update_occupants(self):
+    self.update()  
+
+@classmethod
+def find_neighborhood(cls, neighborhood_id):
+    return cls.objects.filter(id=neighborhood_id)
