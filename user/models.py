299138b0d327232e75b7c14366dfcc747c6ad081
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-22 06:52:32 +0300

    create a post model class for user

--- user/models.py
+++ user/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from hood.models import *
-from django.forms import CharField
+
 # Create your models here.
 
 
 class Profile(models.Model):
+  email = models.EmailField(max_length=100)
   name = models.CharField(max_length=200)
   user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
   neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
@@ -14,3 +15,17 @@ class Profile(models.Model):
 
 def __str__(self):
   return f'{self.user.username} Profile'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_owner')
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post')
+    
+            
+    def __str__(self):
+        return f'{self.title} Post'
+    
+    def delete_post(self):
+        self.delete()
