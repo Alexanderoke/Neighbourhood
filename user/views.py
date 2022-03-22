7b9b0a0d4718b601a8c05741f7b57a887e1c58ce
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-22 07:07:43 +0300

    add register, profile and loginview functions on user views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+  return render(request,'user/register.html')
+
+
+def login(request):
+  return render(request,'user/login.html')  
+
+
+def profile(request):
+  return render(request, 'user/profile.html')  
