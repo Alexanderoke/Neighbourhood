d53f26d86fe7019cc267666ca3cf81e472eaf98c
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-22 06:02:53 +0300

    create admin foreign key for neighbourhood model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NeighbourHood(models.Model):
+  NeighbourhoodName= models.CharField(max_length=100)
+  NeighbourHoodLocation = models.CharField(max_length=200)
+  OccupantsCount = models.IntegerField()
+  admin = models.ForeignKey(User, on_delete=models.CASCADE, related_NeighbourhoodName="hood",null=True)
