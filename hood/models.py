02c262bb43f91274e90877aabb817e8f53398096
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-22 06:45:15 +0300

    install pillow

--- hood/models.py
+++ hood/models.py
@@ -8,7 +8,7 @@ class NeighbourHood(models.Model):
   NeighbourhoodName= models.CharField(max_length=100)
   NeighbourHoodLocation = models.CharField(max_length=200)
   OccupantsCount = models.IntegerField()
-  admin = models.ForeignKey(User, on_delete=models.CASCADE, related_NeighbourhoodName="hood",null=True)
+  admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hood",null=True)
 
 
 def __str__(self):
